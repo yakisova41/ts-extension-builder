235e6a7b77055f563fe5974c4711cff44f7ed801
yakisova41 <yakisoba5emon@gmail.com>
2023-06-20 15:13:29 +0000

    🔖 add release

--- dist/types/config-type.d.ts
+++ dist/types/config-type.d.ts
@@ -7,6 +7,7 @@ export interface TsExtensionBuilderConfig {
     manifest?: JSONSchemaForGoogleChromeExtensionManifestFiles;
     locales?: Locales;
     assetsDir?: string;
+    noSandbox?: boolean;
 }
 export interface DevServer {
     port: number;
